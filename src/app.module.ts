792267d7d530124c1022873c4f1ef20466850841
Muzamil Nizam <muzamil_nizam@epam.com>
2023-08-08 17:44:02 +0200

    cat api added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './controllers/cats/cats.controller';
+import { CatsModule } from './cats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [CatsModule],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
